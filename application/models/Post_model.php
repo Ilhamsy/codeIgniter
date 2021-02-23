ed4b76357a2fca028efe1eece7cd2792a019d2ff
Ilhamsy <ilhamsyah574@gmail.com>
2021-02-23 14:37:33 +0700

    initial commit

--- application/models/Post_model.php
+++ application/models/Post_model.php
@@ -5,12 +5,12 @@
                 'judul'=> $this->input->post('judul'),
                 'sinopsis' => $this->input->post('sinopsis')
             );
-            $this->db->insert('sinopsis',$data);
+            $this->db->insert('sinopsis_dongeng',$data);
         }
         public function getAllPost() {
             return $this->db
             ->select("id_sinopsis,judul,SUBSTRING(sinopsis,1,150) as sinopsis")
-            ->get('sinopsis')
+            ->get('sinopsis_dongeng')
             ->result_array();
         }
         public function getPosts($limit, $start, $keyword=null) {
@@ -19,18 +19,18 @@
             ->select("id_sinopsis,judul,SUBSTRING(sinopsis,1,150) as sinopsis")
             ->like('judul', $keyword)
             ->order_by('id_sinopsis','asc')
-            ->get('sinopsis', $limit, $start)
+            ->get('sinopsis_dongeng', $limit, $start)
             ->result_array();
             
         }
         public function countPosts($keyword=null) {
-            return $this->db->like('judul', $keyword)->from('sinopsis')->count_all_results();
+            return $this->db->like('judul', $keyword)->from('sinopsis_dongeng')->count_all_results();
         }
         public function getPostById($id) {
             return $this->db
             ->select("id_sinopsis,judul,sinopsis")
             ->where('id_sinopsis',$id)
-            ->get('sinopsis')
+            ->get('sinopsis_dongeng')
             ->result_array();
         }
         public function updatePost($id) {
@@ -39,16 +39,16 @@
                 'sinopsis'=>$this->input->post('sinopsis')
             );
 
-            $this->db->where('id_sinopsis', $id)->update('sinopsis', $data);
+            $this->db->where('id_sinopsis', $id)->update('sinopsis_dongeng', $data);
         }
         public function hapusPost($id) {
             $this->db
             ->where('id_sinopsis',$id)
-            ->delete('sinopsis');
+            ->delete('sinopsis_dongeng');
         }
         public function lihat($id) {
             return $this->db
             ->where('id_sinopsis',$id)
-            ->get('sinopsis')->result_array();
+            ->get('sinopsis_dongeng')->result_array();
         }
     }
